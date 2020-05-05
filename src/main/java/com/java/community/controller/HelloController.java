e6558373e63d237909fa446a4c7db5e89dabbc6d
terrorblade1 <1412968676@qq.com>
2020-05-05 19:28:59 +0800

    Initial commit

---
+++ src/main/java/com/java/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.java.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Author: yk
+ * Date: 2020/5/5 18:24
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
